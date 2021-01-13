6e8e146370f91957fd553b68911a1a0c3b853741
prafulpathare <patharepraful@gmail.com>
2021-01-14 01:04:03 +0530

    product data cleaning and product approve API

--- src/main/java/com/shopmart/ShopmartApiApplication.java
+++ src/main/java/com/shopmart/ShopmartApiApplication.java
@@ -1,7 +1,10 @@
 package com.shopmart;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +12,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
 @SpringBootApplication
 public class ShopmartApiApplication implements CommandLineRunner  {
+
+	@Autowired private com.shopmart.config.MongoConfig mongoConfig;
 	
 	private static final Logger log = LoggerFactory.getLogger(ShopmartApiApplication.class);
 	
@@ -34,5 +43,10 @@ public class ShopmartApiApplication implements CommandLineRunner  {
     public void run(String[] args) throws Exception {
 
 		log.info("Application Started");
+		FindIterable<Document> products = mongoConfig.productCollection().find();
+		for (Document document : products) {
+			 mongoConfig.productCollection().updateOne(Filters.eq("_id", new ObjectId(document.getObjectId("_id").toString())), Updates.setOnInsert("is_approved", false));
+			 mongoConfig.productCollection().updateOne(Filters.eq("_id", new ObjectId(document.getObjectId("_id").toString())), Updates.setOnInsert("views", 0));
+		}
 	}
 }
--- src/main/java/com/shopmart/controller/ProductController.java
+++ src/main/java/com/shopmart/controller/ProductController.java
@@ -4,9 +4,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +29,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import com.shopmart.model.Product;
 import com.shopmart.repository.ProductRepository;
 import com.shopmart.service.SupplierService;
@@ -31,12 +40,16 @@ import com.shopmart.service.SupplierService;
 @RequestMapping(value = "/product")
 public class ProductController {
 
-//	@Autowired private ProductService productService;
 	@Autowired private ProductRepository productRepository;
 	@Autowired private SupplierService supplierService;
 	@Autowired private com.shopmart.config.MongoConfig mongoConfig;
 	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
+	@GetMapping(value = "/approve/{product_id}")
+	public ResponseEntity<?> approve(@PathVariable(name = "product_id") String productId){
+	      mongoConfig.productCollection().updateOne(Filters.eq("_id", new ObjectId(productId)), Updates.set("is_approved", true));
+	      return ResponseEntity.status(200).body(null);
+	}
 
 	@PostMapping(value = "")
 	public ResponseEntity<?> createProduct(
@@ -46,22 +59,43 @@ public class ProductController {
 		if(files.length < 3) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("NOT_ENOUGH_FILES");
 		
     	JSONObject productObject = new JSONObject(product);
-    	if(productObject.getString("name") == null || productObject.getString("name").length() < 5) ResponseEntity.status(HttpStatus.BAD_REQUEST).body("NO_OR_BAD_PRODUCT_NAME");
-    	if(String.valueOf(productObject.getDouble("price")) == null || productObject.getDouble("price") < 100.00) ResponseEntity.status(HttpStatus.BAD_REQUEST).body("NO_OR_BAD_PRODUCT_PRICE"); 
+    	try {
+    		if(productObject.getString("name").length() < 5 ||
+				productObject.getDouble("price") < 100.00 ||
+				Arrays.asList("electronics", "appearel").stream().anyMatch(category -> category.equals(productObject.getString("category"))) == false ||
+				productObject.getJSONArray("description").length() < 3
+	    	) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HashMap<String, String>() {{
+	    		put("message", "bad or insufficient data");
+	    	}}); 
+    	}
+    	catch (JSONException e) {
+    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HashMap<String, String>() {{
+	    		put("message", "bad or insufficient data");
+	    	}});
+		}
     	
     	
 		// upload files and save data
-		try {
-			String fullPath = "/opt/lampp/htdocs/cdn.shopmart/products/" + String.valueOf(supplierService.get().getUser_id());
-			Files.createDirectories(Paths.get(fullPath));
-            for (MultipartFile file : files) {
-            	Path copyLocation = Paths.get(fullPath + "/" + UUID.randomUUID().toString().replaceAll("-", "") + ".jpg");				
-            	Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);            	
-			}
-
-        	// save in mongodb
+		try {			
         	Document productForMongo = Document.parse(product);
-        	mongoConfig.productCollection().insertOne(productForMongo);
+        	productForMongo.append("views", 0);
+        	
+        	String fullPath = "/opt/lampp/htdocs/cdn.shopmart/products/" + String.valueOf(supplierService.get().getUser_id());
+        	Files.createDirectories(Paths.get(fullPath));
+        	
+        	List<String> imgurls = new ArrayList<>();
+        	
+            for (MultipartFile file : files) {
+            	String fileName = UUID.randomUUID().toString().replaceAll("-", "") + ".jpg";
+            	Path copyLocation = Paths.get(fullPath + "/" + fileName);				
+            	Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+            	imgurls.add(fileName);
+			}
+            
+        	// save in mongodb
+            productForMongo.append("is_approved", false);
+            productForMongo.append("imgurl", imgurls);
+            mongoConfig.productCollection().insertOne(productForMongo);
         	
         	// save in mysql
         	productRepository.save(new Product(productForMongo.getObjectId("_id").toString(), supplierService.get()));
